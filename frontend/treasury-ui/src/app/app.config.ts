4b4a4872f093106168fe5e8eaf98310844fb3fa9
Tokam <jbtokam23@gmail.com>
2026-04-16 23:17:53 -0400

    Updated config file

--- frontend/treasury-ui/src/app/app.config.ts
+++ frontend/treasury-ui/src/app/app.config.ts
@@ -3,6 +3,13 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatInputModule } from '@angular/material/input';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [provideRouter(routes), provideHttpClient(), provideAnimations(), provideAnimationsAsync()
+  ]
 };
